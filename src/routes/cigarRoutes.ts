d7f5683d87d09fccbc6a2e9cdd04ebf8328efeb0
Slav <Bassclef711@gmail.com>
2025-10-16 15:10:30 -0600

    Update cigarRoutes.ts

--- src/routes/cigarRoutes.ts
+++ src/routes/cigarRoutes.ts
@@ -9,19 +9,15 @@ import {
 
 const router = express.Router();
 
-// GET all cigars
 router.get('/', getAllCigars);
 
-// GET cigar by ID
 router.get('/:id', getCigarById);
 
-// POST new cigar
 router.post('/', createCigar);
 
-// PUT update cigar
 router.put('/:id', updateCigar);
 
-// DELETE cigar
 router.delete('/:id', deleteCigar);
 
 export default router;
+
